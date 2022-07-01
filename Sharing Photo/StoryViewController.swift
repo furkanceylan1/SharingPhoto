17210dc4f91c37fb54b649b239d0f99d29be074a
Furkan <furkn.ceyln@gmail.com>
2022-07-01 19:24:28 +0300

    Final fixes made.

--- Sharing Photo/StoryViewController.swift
+++ Sharing Photo/StoryViewController.swift
@@ -13,9 +13,7 @@ class StoryViewController: UIViewController, UITableViewDelegate, UITableViewDat
 
     @IBOutlet weak var tableView: UITableView!
     
-    var emailArray = [String]()
-    var imageUrlArray = [String]()
-    var commentArray = [String]()
+    var postArray = [Post]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,43 +27,38 @@ class StoryViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return emailArray.count
+        return postArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellStory", for: indexPath) as! StoryCell
-        cell.emailLabel.text = emailArray[indexPath.row]
-        cell.commentLabel.text = commentArray[indexPath.row]
-        cell.storyImageView.sd_setImage(with: URL(string: self.imageUrlArray[indexPath.row]))
+        cell.emailLabel.text = postArray[indexPath.row].email
+        cell.commentLabel.text = postArray[indexPath.row].comment
+        cell.storyImageView.sd_setImage(with: URL(string: self.postArray[indexPath.row].image))
         return cell
     }
     
     @objc func getData(){
         let db = Firestore.firestore()
         
-        db.collection("Post").addSnapshotListener { snapshot, error in
+        db.collection("Post").order(by: "date", descending: true).addSnapshotListener { snapshot, error in
             if error != nil{
                 // Error message
             }else{
                 if snapshot != nil && snapshot?.isEmpty != true {
                     
-                    self.imageUrlArray.removeAll(keepingCapacity: false)
-                    self.emailArray.removeAll(keepingCapacity: false)
-                    self.commentArray.removeAll(keepingCapacity: false)
+                    self.postArray.removeAll(keepingCapacity: false)
                     
                     for document in snapshot!.documents{
                         let documentID = document.documentID
                         
                         if let imageUrl = document.get("image") as? String{
-                            self.imageUrlArray.append(imageUrl)
-                        }
-                        
-                        if let email = document.get("email") as? String{
-                            self.emailArray.append(email)
-                        }
-                        
-                        if let comment = document.get("comment") as? String{
-                            self.commentArray.append(comment)
+                            if let email = document.get("email") as? String{
+                                if let comment = document.get("comment") as? String{
+                                    let post = Post(email: email, comment: comment, image: imageUrl)
+                                    self.postArray.append(post)
+                                }
+                            }
                         }
                     }
                     self.tableView.reloadData()
